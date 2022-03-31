c052b1e12fed84fc1755bfe19df694b609cde5d1
mounikateju05 <tejumounika3@gmail.com>
2022-03-31 18:56:47 +0530

    rearranged

---
+++ dices/index.js
@@ -0,0 +1,12 @@
+ let a;
+ let b;
+ let c;
+document.getElementById("rollBtn").onclick=function()
+{
+   a =  Math.floor(Math.random()*6 )+ 1;
+   b =  Math.floor(Math.random()*6 )+ 1;
+   c =  Math.floor(Math.random()*6 )+ 1;
+    document.getElementById("alabel").innerHTML= a;
+    document.getElementById("blabel").innerHTML= b;
+    document.getElementById("clabel").innerHTML= c;
+}
